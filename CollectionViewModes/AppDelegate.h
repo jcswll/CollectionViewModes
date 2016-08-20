26e723af97f3562a1bf81eac3b0747b76c582c41
Josh Caswell <josh@kraken>
2016-08-19 19:44:08 -0700

    Initial Commit

---
+++ CollectionViewModes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionViewModes
+//
+//  Created by Joshua Caswell on 8/19/16.
+//  Copyright © 2016 Josh Caswell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionViewModes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionViewModes
+//
+//  Created by Joshua Caswell on 8/19/16.
+//  Copyright © 2016 Josh Caswell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
